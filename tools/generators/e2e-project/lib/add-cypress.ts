05cbfce5243064bf5f572a7c35968fcc10de2045
Roger Ramos <rodgerpaulo@hotmail.com>
2022-07-28 15:59:22 -0300

    refactor: Remove e2e sufix As soon as all projects here are e2e ;)

--- tools/generators/e2e-project/lib/add-cypress.ts
+++ tools/generators/e2e-project/lib/add-cypress.ts
@@ -5,7 +5,7 @@ import { Schema } from '../schema';
 export async function addCypress(host: Tree, options: Schema) {
   return await cypressProjectGenerator(host, {
     ...options,
-    name: `${options.name}-e2e`,
+    name: options.name,
     baseUrl: options.projectUrl
   });
 }
